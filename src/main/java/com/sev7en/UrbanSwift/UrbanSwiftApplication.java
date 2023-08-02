1a0d0408f7823e325b8fd328c7e231a1ac6d2681
Mahaveer prasad panigrahi <psatyam86619@gmail.com>
2023-08-02 18:14:40 +0530

    First Commit

---
+++ src/main/java/com/sev7en/UrbanSwift/UrbanSwiftApplication.java
@@ -0,0 +1,13 @@
+package com.sev7en.UrbanSwift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanSwiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbanSwiftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sev7en/UrbanSwift/UrbanSwiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sev7en.UrbanSwift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanSwiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
